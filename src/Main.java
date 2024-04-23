a83eb95861b0e759e865bd1249a3949e87acb1a3
Violetta Sabitova <violetsabitova@gmail.com>
2024-04-23 15:50:16 +0700

    Code v.1.1

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
         double ticket_cost = 13676; //переменная для стоимости билета. тип double выбран т.к. стоимость может быть не целым числом
 
-        int miles = (int) ticket_cost/20;
+        int miles = (int) ticket_cost / 20; //переменная для вычисления бонусных миль. тип int, т.к. по условию задачи мили - целое число
 
-        System.out.println("За билет стоимостью "+ ticket_cost + " руб. будет начислено " + miles + " миль");
+        System.out.println("За билет стоимостью " + ticket_cost + " руб. будет начислено " + miles + " миль");
     }
 }
